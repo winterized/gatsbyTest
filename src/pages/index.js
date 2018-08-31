90460d439adeab45ea7df83c84c01f51459fd6d2
Aurélien FONTAINE <aurelien100mo@yahoo.fr>
2018-08-31 17:00:45 +0200

    Test avec une image dans un container sur la home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import {
+  Container,
+  Image
+} from 'semantic-ui-react'
+import backgroundImage from "../../assets/paris-background.jpg"
 
 const IndexPage = () => (
-  <div>
+  <Container>
+    <Image src={backgroundImage} />
+  </Container>
+  /*<div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  </div>*/
 )
 
 export default IndexPage
